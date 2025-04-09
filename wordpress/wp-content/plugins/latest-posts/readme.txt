f07ea74dcdcdd31f4fff667b08c0c915718435e9
Alexander Silachev <alexander@silachev.com>
2025-04-09 19:40:17 +0200

    chore: add initial file we got from Junior

---
+++ wordpress/wp-content/plugins/latest-posts/readme.txt
@@ -0,0 +1,71 @@
+=== Latest Posts ===
+Contributors: rubel_miah
+Donate link: https://shapedplugin.com
+Tags: latest posts, widget, latest posts widget, posts widget, category posts, latest category posts, post thumbnails
+Requires at least: 4.0
+Tested up to: 6.4.2
+Stable tag: 1.4.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Latest posts widget to display recent posts from category.
+
+== Description ==
+
+<p>Latest Posts is a very powerful widget plugin for WordPress which displays your most recent or latest posts, category based with thumbnails, date. This is perfect for simple blogs & online magazines. Widgets come with highly-customizable control panels. Easy to use and support multi-widget even in the same sidebar.</p>
+
+<h3>Features:</h3>
+<ul>
+	<li>100% Responsive & Mobile Ready</li>
+	<li>Super Easy Installation</li>
+	<li>Minimalist & Lightweight</li>
+	<li>Easily Customizable</li>
+<li>All Major Browsers Supported</li>
+<li>Coded with Pure HTML & CSS</li>
+</ul>
+
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload it to the plugins folder of your blog.
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Now, goto the Widgets page of the Appearance section and configure the Latest Posts.
+
+
+== Screenshots ==
+1. Free Backend.
+2. Free Live view.
+
+
+== Changelog ==
+
+= 1.4.4 Jan 25, 2024 =
+* Tested: WordPress 6.4.2 compatibility.
+
+= 1.4.3 May 25, 2023 =
+* New: A hook 'sp_lpw_date_format' to modify the date format of posts.
+* Fix: The undefined variable warnings.
+* Tested: WordPress 6.2 compatibility.
+
+= 1.4.2 May 28, 2022 =
+* Tested: WordPress 6.0 compatibility.
+
+= 1.4.1 Mar 16, 2021 =
+* Tested: WordPress 5.7 compatibility.
+
+= 1.4 =
+* Fixed deprecated constructor
+
+= 1.3 =
+* Category select option fixed
+
+= 1.2 =
+* WP_Widget Error fixed
+
+= 1.1 =
+* Add class in time and date.
+* Remove . in title.
+
+= 1.0 =
+* Initial Release
---
+++ wordpress/wp-content/themes/valur/js/custom.js
@@ -0,0 +1,106 @@
+/*
+WordPress Hack
+----------------------------------
+Wrap all document in to following.
+jQuery(document).ready(function($) {
+	...
+});
+*/
+jQuery(document).ready(function($) {
+
+$(document).ready(function(e) {
+/* To add and remove active class on clicked li of navigation. */
+var selector = 'nav li';
+
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
+/* END */
+
+/* To add class first and last on first and last of all li. */
+$('ul').children('li:first-child').addClass('first');
+$('ul').children('li:last-child').addClass('last');
+/* END */
+
+});
+/*Bootstrap menu on hover*/
+// Bootstrap menu magic
+  /*$(window).resize(function() {
+    if ($(window).width() < 768) {
+      $(".dropdown-toggle").attr('data-toggle', 'dropdown');
+    } else {
+      $(".dropdown-toggle").removeAttr('data-toggle dropdown');
+    }
+  });*/
+/*Bootstrap menu on hover*/
+/* === Sticky Menu === */
+    (function () {
+        var nav = $('header');
+        var scrolled = false;
+
+        $(window).scroll(function () {
+
+            if (110 < $(window).scrollTop() && !scrolled) {
+                nav.addClass('sticky animated fadeInDown').animate({ 'margin-top': '0px' });
+
+                scrolled = true;
+            }
+
+            if (110 > $(window).scrollTop() && scrolled) {
+                nav.removeClass('sticky animated fadeInDown').css('margin-top', '0px');
+
+                scrolled = false;
+            }
+        });
+    }());
+			
+
+			// Search
+	jQuery( '#search-button' ).click( function () {
+		console.log( "Open Search, Search Centered" );
+		$( "div.mk-fullscreen-search-overlay" ).addClass( "mk-fullscreen-search-overlay-show" );
+	} );
+	jQuery( "a.mk-fullscreen-close" ).click( function () {
+		console.log( "Closed Search" );
+		$( "div.mk-fullscreen-search-overlay" ).removeClass( "mk-fullscreen-search-overlay-show" );
+	} );
+/*Search End*/
+	
+	// Home Page Featured Blog Slider JS Starts	
+
+	$(document).ready(function() {
+  // Swiper: Slider
+      new Swiper('.blogSwiper-container', {
+          loop: true,
+          navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+         },
+      speed: 2000,
+        autoplay: {
+          delay: 5000,
+        },
+		  pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return `<span class="outer-dot swiper-pagination-bullet"><span class="inner-dot"></span></span>`;
+            },
+          },
+          slidesPerView:1,
+          paginationClickable: true,
+          spaceBetween: 20,
+      });
+  });
+	
+	
+$(".single-blog").click(function(){
+	const clickedElement =$(this).find( "a" ).attr("href");
+	window.location.href =  clickedElement;
+  
+});
+	
+	
+	
+});
